c318439bf5d99f126dbe46dc0a57c27d667ffb3c
galwort <wthomasgorbett@gmail.com>
2025-08-10 09:37:52 -0400

    Update scripts to use python3 and add logs to .gitignore

--- jobs/fetch_posts.sh
+++ jobs/fetch_posts.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/tom/Repos/reddit-trends
 source .venv/bin/activate
-py fetch_posts.py >> logs/fetch_posts.log 2>&1
+python3 fetch_posts.py >> logs/fetch_posts.log 2>&1
--- jobs/update_metrics.sh
+++ jobs/update_metrics.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/tom/Repos/reddit-trends
 source .venv/bin/activate
-py update_metrics.py >> logs/update_metrics.log 2>&1
+python3 update_metrics.py >> logs/update_metrics.log 2>&1
