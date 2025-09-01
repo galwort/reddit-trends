e4d18bc6fbee1e42a74001365058b78fbc824b29
galwort <wthomasgorbett@gmail.com>
2025-09-01 09:51:35 -0400

    Fix k error

--- fetch_trends.py
+++ fetch_trends.py
@@ -241,6 +241,8 @@ def cosine_sim(a: np.ndarray, b: np.ndarray) -> float:
 def cluster_window(tags: List[str], X: np.ndarray) -> Dict[int, Dict]:
     if X.size == 0:
         return {}
+    if X.shape[0] < MIN_CLUSTER_SIZE:
+        return {}
     Xn = l2_normalize_matrix(X)
     clusterer = hdbscan.HDBSCAN(min_cluster_size=MIN_CLUSTER_SIZE, metric="euclidean")
     labels = clusterer.fit_predict(Xn)
